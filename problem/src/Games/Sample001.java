c4d337b6c882cbe3280e88e0e7cc9c55ba943944
max3584 <max09204649@gmail.com>
2019-10-04 00:22:07 +0900

    ピラミッド
    ドキュメントの追加

--- problem/src/Games/Sample001.java
+++ problem/src/Games/Sample001.java
@@ -3,9 +3,22 @@ package Games;
 public class Sample001 {
 	public static void main(String... args) {
 		Sample001 debug = new Sample001();
-		System.out.print("Sample")
-		System.out.print(debug.pyramid( 100, 1, new String(), "A","B"));
+		/********************/
+		int n = 100; 					//行数の数
+		int top = 1; 					//トップの列を何列で開始するか
+		String[] mark = {"A", "B"};		//マークの種類
+		/********************/
+
+		System.out.print("Sample");
+		System.out.print(debug.pyramid( n, top, new String(), mark));
 	}
+
+	/**
+	 * @param n			行数の数
+	 * @param top		トップの列を何個にするか
+	 * @param text		再帰を行うための保存Stringバッファ
+	 * @param mark...	2種類以上のマークを付けてください、そうしないとピラミッドの形がきれいに表示されない可能性があります
+	 */
 
 	// Sample
 	public String pyramid(int n, int top, String text, String... mark) {
